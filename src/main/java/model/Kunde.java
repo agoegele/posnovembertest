5680264f755f377fdbb96e1a0c495d4568850663
agoegele <agoegele@tsn.at>
2020-11-18 14:21:09 +0100

    Rechnung geter

--- src/main/java/model/Kunde.java
+++ src/main/java/model/Kunde.java
@@ -1,20 +1,24 @@
 package model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.ArrayList;
 
 
 public class Kunde {
-    private final long id;
-
+    private final int id;
     private final String vorname;
     private final String nachname;
+    private ArrayList<Integer> rechnungen = new ArrayList<Integer>();
 
-    public Kunde(@JsonProperty("id") long id,
+    public Kunde(@JsonProperty("id") int id,
                  @JsonProperty("vorname") String vorname,
-                 @JsonProperty("nachname") String nachname){
+                 @JsonProperty("nachname") String nachname,
+                 @JsonProperty("rechnungen") ArrayList rechnungen){
     this.id=id;
     this.vorname=vorname;
     this.nachname=nachname;
+    this.rechnungen=rechnungen;
+
     }
 
     public long getid(){
@@ -25,6 +29,9 @@ public class Kunde {
     }
     public String getNachname(){
         return nachname;
+    }
+    public ArrayList<Integer> getRechnungen(){
+        return rechnungen;
     }
 
 }
