82b1576744a32f7af5e9fee313baba08ac39c2ad
agoegele <agoegele@tsn.at>
2020-11-18 14:00:49 +0100

    Kundenobjekt geter

--- src/main/java/model/Kunde.java
+++ src/main/java/model/Kunde.java
@@ -4,4 +4,29 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 
 public class Kunde {
+    private final long id;
+
+    private final String vorname;
+    private final String nachname;
+
+    public Kunde(@JsonProperty("id") long id,
+                 @JsonProperty("vorname") String vorname,
+                 @JsonProperty("nachname") String nachname){
+    this.id=id;
+    this.vorname=vorname;
+    this.nachname=nachname;
+    }
+
+    public long getid(){
+        return id;
+    }
+    public String getVorname(){
+        return vorname;
+    }
+    public String getNachname(){
+        return nachname;
+    }
+
 }
+
+
